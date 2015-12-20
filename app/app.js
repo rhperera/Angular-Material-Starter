0af83ca125b5af5eb8b4984de42bd9a8e8f6bf06
rhperera <harith9127@gmail.com>
2015-12-20 12:44:34 +0530

    resources added

---
+++ app/app.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  angular.module('StarterApp',['ngMaterial'])
+
+  .config(function ($stateProvider,$mdThemingProvider,$mdGestureProvider) {
+    // body...
+  });
+  // body...
+}());
