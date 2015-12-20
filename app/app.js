34c9f5ed49e5175882248170a08192b6ba62950b
rhperera <harith9127@gmail.com>
2015-12-21 00:11:51 +0530

    side navs added

--- app/app.js
+++ app/app.js
@@ -1,9 +1,50 @@
 (function () {
   'use strict';
-  angular.module('StarterApp',['ngMaterial'])
+  angular.module('StarterApp',['ngMaterial','ui.router'])
 
-  .config(function ($stateProvider,$mdThemingProvider,$mdGestureProvider) {
-    // body...
-  });
+  .config(['$stateProvider','$mdThemingProvider','$mdGestureProvider',function($stateProvider,$mdThemingProvider,$mdGestureProvider){
+
+  }])
+
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+   $scope.close = function () {
+     $mdSidenav('left').close()
+       .then(function () {
+         $log.debug("close LEFT is done");
+       });
+   };
+ })
+
+ .controller('NavCtrl',function ($scope,$timeout,$mdSidenav,$log) {
+   $scope.toggleLeft = buildDelayedToggler('left');
+
+   function debounce(func, wait, context) {
+    var timer;
+    return function debounced() {
+      var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout(function() {
+        timer = undefined;
+        func.apply(context, args);
+      }, wait || 10);
+    };
+  }
+  /**
+   * Build handler to open/close a SideNav; when animation finishes
+   * report completion in console
+   */
+  function buildDelayedToggler(navID) {
+    return debounce(function() {
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          $log.debug("toggle " + navID + " is done");
+        });
+    }, 200);
+  }
+ })
+
+
   // body...
 }());
